83aa51a5d01d4c228776b7e3b8af5b58b385dd27
DevMandate <wafulacaleb233@gmail.com>
2024-11-14 18:14:09 +0300

    update

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import *
 import pymysql
 
 app = Flask(__name__)
 
-# we are going to create a secret a secret key that will enamble us to secure our session just incase of an attack. The secret key is just randomized string of values
+
 app.secret_key = "2345678oiuytredfghjklytgjhmgfhtfc78ughvnhf"
 
 
@@ -36,7 +36,7 @@ def home():
     cursor2 = connection.cursor()
     cursor2.execute(sql2)
     Smartphones = cursor2.fetchall()
-    render_template("index.html", Electronics = Electronics ,  Smartphones = Smartphones )
+
 
 
     # CLOTHES QUERY
@@ -192,7 +192,126 @@ def upload():
         connection.commit()
         return render_template("upload.html", success = "Product uploaded successfully")
 
+# the imports for the mpesa function
+import requests
+import datetime
+import base64
+from requests.auth import HTTPBasicAuth
 
+@app.route('/mpesapayment', methods=['POST', 'GET'])
+def mpesa_payment():
+    if request.method == 'POST':
+        phone = str(request.form['phone'])
+        amount = str(request.form['amount'])
+        # GENERATING THE ACCESS TOKEN
+        # create an account on safaricom daraja
+        consumer_key = "GTWADFxIpUfDoNikNGqq1C3023evM6UH"
+        consumer_secret = "amFbAoUByPV2rM5A"
+
+        api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"  # AUTH URL
+        r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+
+        data = r.json()
+        access_token = "Bearer" + ' ' + data['access_token']
+
+        #  GETTING THE PASSWORD
+        timestamp = datetime.datetime.today().strftime('%Y%m%d%H%M%S')
+        passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+        business_short_code = "174379"
+        data = business_short_code + passkey + timestamp
+        encoded = base64.b64encode(data.encode())
+        password = encoded.decode('utf-8')
+
+        # BODY OR PAYLOAD
+        payload = {
+            "BusinessShortCode": "174379",
+            "Password": "{}".format(password),
+            "Timestamp": "{}".format(timestamp),
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": amount,  # use 1 when testing
+            "PartyA": phone,  # change to your number
+            "PartyB": "174379",
+            "PhoneNumber": phone,
+            "CallBackURL": "https://modcom.co.ke/job/confirmation.php",
+            "AccountReference": "account",
+            "TransactionDesc": "account"
+        }
+
+        # POPULAING THE HTTP HEADER
+        headers = {
+            "Authorization": access_token,
+            "Content-Type": "application/json"
+        }
+
+        url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"  # C2B URL
+
+        response = requests.post(url, json=payload, headers=headers)
+        print(response.text)
+        return '<h3>Please Complete Payment in Your Phone and we will deliver in minutes</h3>' \
+               '<a href='"/"' class="btn btn-dark btn-sm">Back to Products</a>'
+
+
+@app.route("/allproducts")
+def product():
+    
+        # create a db connection
+        connection = db_connection()
+        # create an sql query. The %s is a placeholder, to be replaced with actual data when we execute the sql
+        sql = "SELECT * FROM `products`"
+        # create a cursor that will help to execute the sql
+        cursor = connection.cursor()
+        # execute the query
+        cursor.execute(sql)
+
+        # fetch one person
+        product = cursor.fetchall()
+        return render_template("productupdate.html", product=product)
+
+
+
+@app.route("/deleteProduct/<product_id>", methods=["POST"])
+def delete_product(product_id):
+    connection = db_connection()
+    cursor = connection.cursor()
+
+    # SQL to delete the user by username
+    sql = "DELETE FROM products WHERE product_id = %s"
+    cursor.execute(sql, (product_id,))
+    connection.commit()
+    
+    return redirect("/allproducts")  # Redirect back to the user list after deletion
+
+
+@app.route("/updateProduct/<product_id>", methods=["GET", "POST"])
+def update_product(product_id):
+    connection = db_connection()
+    cursor = connection.cursor()
+
+    if request.method == "POST":
+        # Get updated data from form
+        product_name = request.form.get('product_name')
+        product_description = request.form.get('product_description')
+        product_price = request.form.get('product_price')
+        product_category = request.form.get('product_category')
+        product_picture = request.form.get('product_picture')
+
+        # Update the user details in the database
+        sql = """
+        UPDATE products 
+        SET product_name = %s, product_desc = %s, product_cost = %s, product_category = %s, product_image_name = %s
+        WHERE product_id = %s
+        """
+        cursor.execute(sql, (product_name, product_description, product_price, product_category, product_picture,  product_id))
+        connection.commit()
+
+        return redirect("/allproducts")  # Redirect to the products list
+
+    # For GET, we display the current user data to be updated
+    sql = "SELECT * FROM products WHERE product_id = %s"
+    cursor.execute(sql, (product_id,))
+    one_product = cursor.fetchone()
+
+    return render_template("product_update_form.html", oneProduct=one_product)
 
 
 
